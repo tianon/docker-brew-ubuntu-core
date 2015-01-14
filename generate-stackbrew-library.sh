f09bc96becf24fc04922aa020f5ba5d0bb346236
You-Sheng Yang <vicamo@gmail.com>
2015-01-14 16:41:13 +0800

    fix generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -11,12 +11,51 @@ noVersion=(
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+get_part() {
+	local dir="$1"
+	shift
+	local part="$1"
+	shift
+	if [ -f "$dir/$part" ]; then
+		cat "$dir/$part"
+		return 0
+	fi
+	if [ -f "$part" ]; then
+		cat "$part"
+		return 0
+	fi
+	if [ $# -gt 0 ]; then
+		echo "$1"
+		return 0
+	fi
+	return 1
+}
+
 versions=( */ )
 versions=( "${versions[@]%/}" )
-url='git://github.com/tianon/docker-brew-ubuntu-core'
+
+tasks=()
+for v in "${versions[@]}"; do
+	arches=( $v/*/ )
+	arches=( "${arches[@]%/}" )
+	arches=( "${arches[@]#$v/}" )
+	for arch in "${arches[@]}"; do
+		dir="$(readlink -f "$v/$arch")"
+
+		skip="$(get_part "$dir" skip '')"
+		if [ -n "$skip" ]; then
+			echo "Skipping $v/$arch, reason: $skip"
+			continue;
+		fi
+
+		tasks+=( $v/$arch )
+	done
+done
+
+url='git://github.com/vicamo/docker-brew-ubuntu-core'
 
 cat <<-'EOH'
-# maintainer: Tianon Gravi <admwiggin@gmail.com> (@tianon)
+# maintainer: You-Sheng Yang <vicamo@gmail.com> (@vicamo)
 
 # see https://partner-images.canonical.com/core/
 # see also https://wiki.ubuntu.com/Releases#Current
@@ -30,8 +69,11 @@ if [ "$commitCount" ] && [ "$commitCount" -gt 0 ]; then
 	git log --format=format:'- %h %s%n%w(0,2,2)%b' "$commitRange" | sed 's/^/#  /'
 fi
 
-arch="$(dpkg --print-architecture)"
-for version in "${versions[@]}"; do
+systemArch="$(dpkg --print-architecture)"
+for task in "${tasks[@]}"; do
+	version=$(echo $task | cut -d / -f 1)
+	arch=$(echo $task | cut -d / -f 2)
+
 	commit="$(git log -1 --format='format:%H' -- "$version/$arch")"
 	serial="$(awk -F '=' '$1 == "SERIAL" { print $2 }' "$version/build-info.txt")"
 	
@@ -43,15 +85,30 @@ for version in "${versions[@]}"; do
 			fullVersion="$(eval "$(tar -xvf "$tarball" etc/os-release --to-stdout 2>/dev/null)" && echo "$VERSION" | cut -d' ' -f1)"
 		fi
 		if [ "$fullVersion" ]; then
-			versionAliases+=( $fullVersion )
+			versionAliases+=( $fullVersion-$arch )
+			if [ "$arch" == "$systemArch" ]; then
+				versionAliases+=( $fullVersion )
+			fi
 			if [ "${fullVersion%.*.*}" != "$fullVersion" ]; then
 				# three part version like "12.04.4"
-				versionAliases+=( ${fullVersion%.*} )
+				versionAliases+=( ${fullVersion%.*}-$arch )
+				if [ "$arch" == "$systemArch" ]; then
+					versionAliases+=( ${fullVersion%.*} )
+				fi
 			fi
 		fi
 	fi
 	# TODO something cool/useful with $serial
-	versionAliases+=( $version ${aliases[$version]} )
+	versionAliases+=( $version-$arch )
+	if [ "$arch" == "$systemArch" ]; then
+		versionAliases+=( $version )
+	fi
+	if [ "x${aliases[$version]}" != "x" ]; then
+		versionAliases+=( ${aliases[$version]}-$arch )
+		if [ "$arch" == "$systemArch" ]; then
+			versionAliases+=( ${aliases[$version]} )
+		fi
+	fi
 	
 	echo
 	echo "# $serial"
