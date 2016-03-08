1070fe8224e2cea46452d8699dc7ee4797746ee7
Tianon Gravi <admwiggin@gmail.com>
2016-03-07 16:11:27 -0800

    Add "set -xe" to our long line so we can see what's going on when it fails

--- update.sh
+++ update.sh
@@ -56,7 +56,9 @@ EOF
 
 # a few minor docker-specific tweaks
 # see https://github.com/docker/docker/blob/master/contrib/mkimage/debootstrap
-RUN echo '#!/bin/sh' > /usr/sbin/policy-rc.d \
+RUN set -xe \
+	\
+	&& echo '#!/bin/sh' > /usr/sbin/policy-rc.d \
 	&& echo 'exit 101' >> /usr/sbin/policy-rc.d \
 	&& chmod +x /usr/sbin/policy-rc.d \
 	\
